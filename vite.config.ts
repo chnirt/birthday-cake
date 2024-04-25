e421a01781138ffe49c031d4d16092ee190dfdb4
Chin Trinh <35022967+chnirt@users.noreply.github.com>
2024-04-26 02:04:40 +0700

    feat: update config

--- vite.config.ts
+++ vite.config.ts
@@ -7,15 +7,20 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        assetFileNames: (assetInfo) => {
-          let extType = assetInfo.name.split(".").at(1);
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            extType = "img";
-          }
-          return `assets/${extType}/[name]-[hash][extname]`;
+        // manualChunks(id) {
+        //   if (id.includes("node_modules")) {
+        //     return id
+        //       .toString()
+        //       .split("node_modules/")[1]
+        //       .split("/")[0]
+        //       .toString();
+        //   }
+        // },
+        manualChunks: {
+          // Specify manual chunk names and modules
+          react: ["react", "react-dom"],
+          lottie: ["react-lottie"],
         },
-        chunkFileNames: "assets/js/[name]-[hash].js",
-        entryFileNames: "assets/js/[name]-[hash].js",
       },
     },
   },
