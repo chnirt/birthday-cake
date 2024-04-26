e23887d62bda2f35d945e7af85f5773f4b329b31
Chin Trinh <35022967+chnirt@users.noreply.github.com>
2024-04-26 11:51:57 +0700

    feat: update threshold

--- src/App.tsx
+++ src/App.tsx
@@ -75,7 +75,7 @@ function App() {
         analyser.getByteFrequencyData(dataArray);
         const average =
           dataArray.reduce((acc, val) => acc + val, 0) / bufferLength;
-        const threshold = 40;
+        const threshold = 100;
 
         if (average > threshold) {
           // setIsBlowing(true);
